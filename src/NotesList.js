7530df8b2f4b3a52e57a64863fff226a3b411a91
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-05 22:40:34 -0300

    Extract "NotesListItem" from "NotesList"

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,17 +1,12 @@
 import React from "react";
 import './scss/NotesList.scss';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import NotesListItem from "./NotesListItem";
 
 function NotesList() {
+    const notes = [1, 2, 3, 4, 5, 6, 7];
     return (
         <ul className="notes_list">
-            <li className="notes_list__item" tabIndex="0">
-                <div className="notes_list__item__header">
-                    <span className="notes_list__item__header__title">Title of the Note</span>
-                    <button className="notes_list__item__header__options"><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
-                </div>
-                <p className="notes_list__item__content">lorem ipsum dolor sit amet</p>
-            </li>
+            { notes.map((n) => <NotesListItem key={n}/>) }
         </ul>
     );
 }
