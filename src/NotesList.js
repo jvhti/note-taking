6b3319e3d921863926ee397dbec2ca9c4ec3f21b
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-11 23:53:48 -0300

    Improve scroll of "NotesList"; Move "NotesListItem" to "NotesList"; Improve .gitignore to ignore .idea;

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,9 +1,21 @@
 import React from "react";
 import './scss/NotesList.scss';
-import NotesListItem from "./NotesListItem";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function NotesListItem() {
+    return (
+        <li className="notes_list__item" tabIndex="0">
+            <div className="notes_list__item__header">
+                <span className="notes_list__item__header__title">Title of the Note</span>
+                <button className="notes_list__item__header__options"><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
+            </div>
+            <p className="notes_list__item__content">lorem ipsum dolor sit amet</p>
+        </li>
+    );
+}
 
 function NotesList() {
-    const notes = [1, 2, 3, 4, 5, 6, 7];
+    const notes = [1, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7];
     return (
         <ul className="notes_list">
             { notes.map((n) => <NotesListItem key={n}/>) }
