2248d09013c3974c22704767396d30ec78d545cd
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-12 00:38:05 -0300

    Implement basic layout of the Switch Mode Button on "NoteEditor"

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.scss';
 import SideNavigation from "./SideNavigation";
 import NoteEditor from "./NoteEditor";
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faEllipsisH, faFileAlt} from '@fortawesome/free-solid-svg-icons';
+import {faEllipsisH, faFileAlt, faGlasses} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faFileAlt);
 library.add(faEllipsisH);
+library.add(faGlasses);
 
 function App() {
   return (
--- src/NoteEditor.js
+++ src/NoteEditor.js
@@ -1,43 +1,8 @@
 import React from "react";
 import './scss/NoteEditor.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const text = "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "# 1\n" +
-    "## 2\n" +
-    "### 3\n" +
-    "\n";
+const text = "# 1\n" + "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +    "## 2\n" +    "### 3\n" +    "# 1\n" +   "## 2\n" +    "### 3\n" +    "\n";
 
 function NoteEditor() {
     return (
@@ -48,6 +13,7 @@ function NoteEditor() {
             <div className="note_editor__editor">
                 <textarea className="note_editor__editor">{text}</textarea>
             </div>
+            <button className="note_editor__switch_mode"><FontAwesomeIcon size="2x" icon="glasses"/><span className="sr-only">Read</span></button>
         </main>
     );
 }
