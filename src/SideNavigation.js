c6d05a1757ce7c5a400347f9944bb49770a401da
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-17 22:38:28 -0300

    Join "NotesList" with "SideNavigation"

--- src/SideNavigation.js
+++ src/SideNavigation.js
@@ -1,18 +1,38 @@
 import React from "react";
 import './scss/SideNavigation.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import NotesList from "./NotesList";
+
+function NotesListItem() {
+    return (
+        <li className="notes_list__item" tabIndex="0">
+            <div className="notes_list__item__header">
+                <span className="notes_list__item__header__title">Title of the Note</span>
+                <button className="notes_list__item__header__options"><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
+            </div>
+            <p className="notes_list__item__content">lorem ipsum dolor sit amet</p>
+        </li>
+    );
+}
+
+function NotesList() {
+    const notes = [1, 2, 3];
+    return (
+        <ul className="notes_list">
+            { notes.map((n) => <NotesListItem key={n}/>) }
+        </ul>
+    );
+}
 
 function SideNavigation() {
     return (
-<aside className="sidebar">
-    <div className="sidebar__options">
-        <input className="sidebar__options__search_bar" type="search" placeholder="Search Notes"/>
-        <button className="sidebar__options__new_note"><FontAwesomeIcon icon="file-alt"/><span className="sr-only">Create new note</span></button>
-    </div>
-    <hr className="sidebar__separator"/>
-    <NotesList/>
-</aside>
+        <aside className="sidebar">
+            <div className="sidebar__options">
+                <input className="sidebar__options__search_bar" type="search" placeholder="Search Notes"/>
+                <button className="sidebar__options__new_note"><FontAwesomeIcon icon="file-alt"/><span className="sr-only">Create new note</span></button>
+            </div>
+            <hr className="sidebar__separator"/>
+            <NotesList/>
+        </aside>
     );
 }
 
