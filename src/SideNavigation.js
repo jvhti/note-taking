9c1db0ed5472a9861c2561ca97e78d56defa543c
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-17 23:22:25 -0300

    Implement "NotesListItemOptions"

--- src/SideNavigation.js
+++ src/SideNavigation.js
@@ -2,7 +2,18 @@ import React from "react";
 import './scss/SideNavigation.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-function NotesListItem() {
+function NotesListItemOptions() {
+    return (
+        <ul className="notes_list__item__options_wrapper">
+            <li className="notes_list__item__options_wrapper__option"><a href="#">Delete</a></li>
+            <li className="notes_list__item__options_wrapper__option"><a href="#">Print</a></li>
+            <li className="notes_list__item__options_wrapper__option"><a href="#">Share</a></li>
+        </ul>
+    );
+}
+
+function NotesListItem(props) {
+    const options = props.isOptionsOpen === true ? <NotesListItemOptions/> : undefined;
     return (
         <li className="notes_list__item" tabIndex="0">
             <div className="notes_list__item__header">
@@ -10,17 +21,25 @@ function NotesListItem() {
                 <button className="notes_list__item__header__options"><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
             </div>
             <p className="notes_list__item__content">lorem ipsum dolor sit amet</p>
+            {options}
         </li>
     );
 }
 
-function NotesList() {
-    const notes = [1, 2, 3];
-    return (
-        <ul className="notes_list">
-            { notes.map((n) => <NotesListItem key={n}/>) }
-        </ul>
-    );
+class NotesList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.notes = [1];
+    }
+
+    render() {
+        return (
+            <ul className="notes_list">
+                {this.notes.map((n) => <NotesListItem isOptionsOpen={true} key={n}/>)}
+            </ul>
+        );
+    }
 }
 
 function SideNavigation() {
