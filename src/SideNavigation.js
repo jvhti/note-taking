f240b8db4d6d84620af754d7f47206e3fe670f7e
João Víctor de Oliveira Santos <jvhti@hotmail.com>
2019-10-18 23:05:15 -0300

    Implement "NotesListItemOptions" basic logic and events

--- src/SideNavigation.js
+++ src/SideNavigation.js
@@ -2,23 +2,23 @@ import React from "react";
 import './scss/SideNavigation.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-function NotesListItemOptions({left, top}) {
-    const style = {left, top};
+function NotesListItemOptions({left, top, display, events}) {
+    const style = {left, top, display};
     return (
         <ul style={style} className="notes_list__item__options_wrapper">
-            <li className="notes_list__item__options_wrapper__option"><a href="#">Delete</a></li>
-            <li className="notes_list__item__options_wrapper__option"><a href="#">Print</a></li>
-            <li className="notes_list__item__options_wrapper__option"><a href="#">Share</a></li>
+            <li onClick={events.onDelete} className="notes_list__item__options_wrapper__option"><a href="#">Delete</a></li>
+            <li onClick={events.onPrint} className="notes_list__item__options_wrapper__option"><a href="#">Print</a></li>
+            <li onClick={events.onShare} className="notes_list__item__options_wrapper__option"><a href="#">Share</a></li>
         </ul>
     );
 }
 
-function NotesListItem() {
+function NotesListItem(props) {
     return (
         <li className="notes_list__item" tabIndex="0">
             <div className="notes_list__item__header">
                 <span className="notes_list__item__header__title">Title of the Note</span>
-                <button className="notes_list__item__header__options" onClick={(ev) => {console.log(ev.pageX, ev.pageY);}}><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
+                <button className="notes_list__item__header__options" onClick={(ev) => {props.onOpenOptions(ev);}}><FontAwesomeIcon icon="ellipsis-h"/><span className="sr-only">Options for note Title of the Note</span></button>
             </div>
             <p className="notes_list__item__content">lorem ipsum dolor sit amet</p>
         </li>
@@ -29,14 +29,72 @@ class NotesList extends React.Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            options: {
+                display: "none",
+                x: 222,
+                y: 84
+            }
+        };
+
         this.notes = [1];
+
+        this.closeOptions = this.closeOptions.bind(this);
     }
+
+    openOptions(key, ev){
+        const newState = {
+            ...this.state,
+            options : {
+                    ...this.state.options,
+                    x: ev.clientX,
+                    y: ev.clientY,
+                    display: this._toggleOptionsVisibility()
+            }
+        };
+
+        this.setState(newState);
+
+        if (newState.options.display === "block")
+            setTimeout(() => {
+                window.addEventListener("click", this.closeOptions);
+            }, 1);
+    };
+
+    _toggleOptionsVisibility(){
+        return this.state.options.display === "block" ? "none" : "block";
+    }
+
+    closeOptions() {
+        if (this.state.options.display === "block")
+            this.setState({
+                ...this.state,
+                options: {
+                    ...this.state.options,
+                    display: "none"
+                }
+            });
+
+        window.removeEventListener("click", this.closeOptions);
+    }
+
+    onOptionsDelete(ev){console.log("DELETE", ev);}
+    onOptionsPrint(ev){console.log("PRINT", ev);}
+    onOptionsShare(ev){console.log("SHARE", ev);}
 
     render() {
         return (
-            <ul className="notes_list">
-                {this.notes.map((n) => <NotesListItem key={n}/>)}
-            </ul>
+            <React.Fragment>
+                <ul className="notes_list">
+                    {this.notes.map((n) => <NotesListItem onOpenOptions={this.openOptions.bind(this, n)} key={n}/>)}
+                </ul>
+                <NotesListItemOptions events={{
+                    onDelete: this.onOptionsDelete,
+                    onPrint: this.onOptionsPrint,
+                    onShare: this.onOptionsShare
+                }} display={this.state.options.display} left={this.state.options.x} top={this.state.options.y}/>
+                {/* Can pick left and top offset from ev.pageX and ev.pageY */}
+            </React.Fragment>
         );
     }
 }
@@ -50,7 +108,6 @@ function SideNavigation() {
             </div>
             <hr className="sidebar__separator"/>
             <NotesList/>
-            <NotesListItemOptions left={222} top={84}/> {/* Can pick left and top offset from ev.pageX and ev.pageY */}
         </aside>
     );
 }
