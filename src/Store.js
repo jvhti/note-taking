ceb8daf92e70d6edb8ce3f199b10078ea548f0eb
João Víctor <jvhti@hotmail.com>
2019-11-24 15:12:03 -0300

    Remove Redux Thunk;

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,11 @@
-import {applyMiddleware, createStore} from 'redux';
+import {createStore} from 'redux';
 import rootReducer from './Reducers';
 import {changeNote} from './Actions';
 import NoteManager from "./NoteManager";
-import ReduxThunk from 'redux-thunk';
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(rootReducer, {}, applyMiddleware(ReduxThunk));
+let store = createStore(rootReducer);
 
 store.subscribe(() => console.log(store.getState()));
 
